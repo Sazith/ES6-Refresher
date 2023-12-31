4f5a923bb1f21fc8a89f9942170928f69211ba98
Sazith <sazith.simecsoftdev@gmail.com>
2023-12-31 15:41:03 +0600

    variable add

---
+++ Variables.js
@@ -0,0 +1,26 @@
+// var
+var a = 45;
+console.log(a);
+var a = 'sazith';
+console.log(a);
+
+//let
+
+let b = 35;
+console.log(b);
+
+/* SyntaxError: Identifier 'b' has already been declared
+let b= 'shyonton';
+
+*/
+b = 'Shyonton'
+console.log(b);
+
+//const 
+const c = 10;
+console.log(c);
+
+/* SyntaxError: Identifier 'b' has already been declared
+const c = 'sharon'
+
+*/
