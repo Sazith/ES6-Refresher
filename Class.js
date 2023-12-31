aa13abf3b6817a82db75b5814144ee08c8ab8a60
Sazith <sazith.simecsoftdev@gmail.com>
2023-12-31 16:04:45 +0600

    class add

---
+++ Class.js
@@ -0,0 +1,34 @@
+// Classes - Blueprint to create objects
+
+class Vehicle {
+  myVar = 'Yes';
+  constructor(param1, param2){
+    this.color = param1;
+    this.passenger = param2;
+  }
+  saySomething(){
+    console.log(this.color);
+  }
+}
+
+let obj1 = new Vehicle('Red',4);
+let obj2 = new Vehicle('Blue',2);
+
+console.log(obj1);
+console.log(obj2);
+
+obj1.saySomething();
+obj2.saySomething();
+
+// Inheritance
+
+// Derived class
+class Car extends Vehicle{
+  constructor(numWheel,pass,color){
+    super(color,pass);  
+    this.wheel = numWheel;
+  }
+}
+
+let obj3 = new Car(4,6,'Yellow');
+console.log(obj3)
